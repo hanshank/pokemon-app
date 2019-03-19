166211053126f6d28fe3469624f88b6e9cc8f22c
Kevin Amarbayar <amarbayar.kevin@gmail.com>
2019-03-19 13:36:43 -0700

    navbar 1.0

---
+++ src/hideSearch.js
@@ -0,0 +1,11 @@
+function hideSearch() {
+    var x = document.getElementsById("search-container");
+    
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+    return
+    }
+    
+  }
+}
